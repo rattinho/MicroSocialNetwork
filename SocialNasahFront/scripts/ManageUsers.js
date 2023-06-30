f0b7bdfc67142a1746f218a8d8a0155825cc0377
Felipe Ratto <felipeguimaraesratto@gmail.com>
2023-06-30 02:28:56 -0300

    Change variable that decides if run in docker

--- SocialNasahFront/scripts/ManageUsers.js
+++ SocialNasahFront/scripts/ManageUsers.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dbuser = process.env.BDUSER
 const dbpass = process.env.BDPASS
-const docker = false
+const docker = true
 
 if(docker){
     mongoose.connect('mongodb://'+dbuser+':'+dbpass+'@sndb:27017/socialnasah?authSource=admin', {
